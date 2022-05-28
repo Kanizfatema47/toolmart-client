b8e96d48cc772096a17f4855c0ef5a04fef8a1c7
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2022-05-28 21:00:44 +0600

    check out form done with stripe

--- src/Pages/CheckoutForm.js
+++ src/Pages/CheckoutForm.js
@@ -1,11 +1,133 @@
-import React from 'react';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useEffect, useState } from 'react';
 
-const CheckoutForm = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const CheckoutForm = ({data}) => {
+
+
+   const stripe = useStripe();
+   const elements = useElements();
+   const [cardError, setCardError] = useState("");
+   const [success, setSuccess] = useState("");
+   const [processing, setProcessing] = useState(false);
+   const [transactionId, setTransactionId] = useState("");
+   const [clientSecret, setClientSecret] = useState("");
+
+   const { _id, price, name, email } = data;
+   var newPrice = price.toString();
+   console.log(typeof(newPrice))
+
+
+    useEffect(() => {
+      fetch("http://localhost:5000/create-payment-intent", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: JSON.stringify({ newPrice }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data?.clientSecret) {
+            setClientSecret(data.clientSecret);
+          }
+        });
+    }, [newPrice]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+
+     if (!stripe || !elements) {
+            return;
+        }
+
+        const card = elements.getElement(CardElement);
+
+        if (card === null) {
+            return;
+        }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
+
+      
+            setCardError(error?.message || "");
+            setSuccess("");
+            setProcessing(true);
+
+        const { paymentIntent, error: intentError } =
+          await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+              card: card,
+              billing_details: {
+                name: name,
+                email: email,
+              },
+            },
+          });
+         if (intentError) {
+           setCardError(intentError?.message);
+           setProcessing(false);
+         }else{
+            setCardError('');
+            setTransactionId(paymentIntent.id);
+           setSuccess('Congrats')
+         }
+
+           const payment = {
+             data: _id,
+             transactionId: paymentIntent.id,
+           };
+           fetch(`http://localhost:5000/order/${_id}`, {
+             method: "PATCH",
+             headers: {
+               "content-type": "application/json",
+               authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+             },
+             body: JSON.stringify(payment),
+           })
+             .then((res) => res.json())
+             .then((data) => {
+               setProcessing(false);
+               console.log(data);
+             });
+
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-success btn-sm mt-4"
+          type="submit"
+          disabled={!stripe}
+        >
+          Pay
+        </button>
+      </form>
+      {cardError && <p className='text-red-500'>{cardError}</p>}
+      {success && <p className='text-green-500'>{success}</p>}
+      {transactionId && <p>Your {transactionId}</p>}
+    </>
+  );
 };
 
 export default CheckoutForm;
